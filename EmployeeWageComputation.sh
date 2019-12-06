fdfce8aa1c690c1413cf6d2f2f853248a258333f
jotirambandgar <bandgar09@gmail.com>
2019-12-06 18:10:55 +0530

    added: checking employee is present or absent

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,2 +1,10 @@
 #!/bin/bash -x
-echo "Welcome to Employee Wage Comutation"
+	echo "Welcome to Employee Wage computation"
+	tempRandom=$(( RANDOM%2 ))
+	if [ $tempRandom -eq 1 ]
+		then
+			echo "employee present"
+	else
+			echo "employee absent"
+	fi
+
