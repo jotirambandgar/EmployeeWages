38444e51da4bffd101832bc2071daafb1f6caf22
jotirambandgar <bandgar09@gmail.com>
2019-12-07 12:39:15 +0530

    added : wages for 20 days

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,14 +1,34 @@
 #!/bin/bash -x
 	echo "Welcome to Employee Wage computation"
-	TEMP_RANDOM=$(( RANDOM%2 ))
+	#xTEMP_RANDOM=$(( RANDOM%2 ))
 	FULLDAY_WORKING_HOURS=8
 	HALFDAY_WORKING_HOURS=4
 	WAGES=20
-	if [ $TEMP_RANDOM -eq 1 ]
+	FULL_DAY=1
+	PART_TIME=0
+	WORKING_MONTH_DAY=20
+	counter=0;
+	for(( i=0 ; i<20 ;i++)) 
+	do
+	tempRandom=$(( RANDOM%2 ))
+
+	if [ $tempRandom -eq 1 ]
 		then
 			echo "employee present"
-			dailyWage=$(( $FULLDAY_WORKING_HOURS * $WAGES ))
+			timeStatus=$(( RANDOM%2 ))
+			case $timeStatus in
+						$FULL_DAY )
+									dailyWage=$(( $FULLDAY_WORKING_HOURS * $WAGES ))
+									;;
+						$PART_TIME )
+									 dailyWage=$(( $HALFDAY_WORKING_HOURS * $WAGES ))
+									;;
+			esac
 	else
+			dailyWage=0
 			echo "employee absent"
 	fi
-
+			echo "wage of day $i = $dailyWage "
+			echo "$dailyWage"
+			monthlyWage=$(( $dailyWage * $WORKING_MONTH_DAY ))
+	done
