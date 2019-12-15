03fd92da49b7e09405fc9e524c47502ac463b7c8
jotirambandgar <bandgar09@gmail.com>
2019-12-16 00:15:38 +0530

    Modified : remove commented of code and improve indentation

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,20 +1,20 @@
  #!/bin/bash -x
 	echo "Welcome to Employee Wage computation"
-	#xTEMP_RANDOM=$(( RANDOM%2 ))
 	FULLDAY_WORKING_HOURS=8
 	HALFDAY_WORKING_HOURS=4
 	WAGES=20
 	FULL_DAY=1
 	PART_TIME=0
 	WORKING_MONTH_DAY=20
+
 	counter=0;
 	day=0
 	workingHours=100
-	counter=0
 	totalWage=0
 	declare -A monthlyWage;
-function getWorkingHrs(){
- timeStatus=$(( RANDOM%2 ))
+
+	function getWorkingHrs(){
+ 		timeStatus=$(( RANDOM%2 ))
          case $timeStatus in
                   $FULL_DAY )
                            echo $FULLDAY_WORKING_HOURS 
@@ -23,10 +23,11 @@ function getWorkingHrs(){
                             echo $HALFDAY_WORKING_HOURS 
                            ;;
 			esac
-}
+	}
+
 	while [[ $WORKING_MONTH_DAY -ne 0 && $workingHours -gt 0 ]] 
 	do
-	tempRandom=$(( RANDOM%2 ))
+		tempRandom=$(( RANDOM%2 ))
 
 	if [ $tempRandom -eq 1 ]
 		then
@@ -43,12 +44,8 @@ function getWorkingHrs(){
 			echo "wage of day $day = $dailyWage "
 			echo "$workingHours"
 			WORKING_MONTH_DAY=$(( $WORKING_MONTH_DAY - 1 ))
-			#dailyWages[(COUNTER++)]=$dailyWage
-			#echo "counter--->$counter"
 			totalWage=$(( $totalWage + $dailyWage ))
-			#totalWage[($COUNTER)]=$TOTAL_WAGE
 			dailyTotalWage="$dailyWage  $totalWage"
 			monthlyWage[$dayKey]=$dailyTotalWage
 	done
 	echo "${monthlyWage[DAY 1]}"
-	#echo "${dailyWages[@]} daily"
